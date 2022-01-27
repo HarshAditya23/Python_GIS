aac9b42d6b7c50faff52ab75382c04c98d394e3f
HarshAditya23 <34307686+HarshAditya23@users.noreply.github.com>
2022-01-27 18:43:15 +0530

    Temp. Calculation for Urban and Rural

--- Daytime Analysis MODIS.py
+++ Daytime Analysis MODIS.py
@@ -1,3 +1,9 @@
+"""
+Created on Thu Jan 27 18:18:13 2022
+
+@author: gaurh
+"""
+
 # Earth Engine Library
 import ee
 from temperature import transform
@@ -26,6 +32,38 @@ Rural_poi = ee.Geometry.Point(Rural_lon, Rural_lat)
 # Resolution in meters
 
 Scale = 1000
+
+# Mean, Min and Max temperature of Urban POI
+Urban_mean = Temp.mean().sample(Urban_poi, Scale).first(
+        ).get('LST_Day_1km').getinfo()
+Urban_min = Temp.min().sample(Urban_poi, Scale).first(
+        ).get('LST_Day_1km').getinfo()
+Urban_max = Temp.max().sample(Urban_poi, Scale).first(
+        ).get('LST_Day_1km').getinfo()
+
+# Temperature in degree celcius
+print('Mean daytime LST at Urban POI:', round(Urban_poi * 0.02 - 273.15, 2),
+      '°C')
+print('Minimum daytime LST at Urban POI:', round(Urban_poi * 0.02 - 273.15, 2),
+      '°C')
+print('Maximum daytime LST at Urban POI:', round(Urban_poi * 0.02 - 273.15, 2),
+      '°C')
+
+# Mean, Min and Max temperature of Rural POI
+Rural_mean = Temp.mean().sample(Rural_poi, Scale).first(
+        ).get('LST_Day_1km').getinfo()
+Rural_min = Temp.mean().sample(Rural_poi, Scale).first(
+        ).get('LST_Day_1km').getinfo()
+Rural_max = Temp.mean().sample(Rural_poi, Scale).first(
+        ).get('LST_Day_1km').getinfo()
+
+# Temperature in degree celcius
+print('Mean daytime LST at Rural POI:', round(Rural_poi * 0.02 - 273.15, 2),
+      '°C')
+print('Minimum daytime LST at Rural POI:', round(Rural_poi * 0.02 - 273.15, 2),
+      '°C')
+print('Maximum daytime LST at Rural POI:', round(Rural_poi * 0.02 - 273.15, 2),
+      '°C')
 
 Urban_df = transform(Urban_poi, ['LST_Day_1km'])
 Rural_df = transform(Rural_poi, ['LST_Day_1km'])
